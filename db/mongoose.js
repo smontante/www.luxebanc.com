21fe9f30532ea1cec6a1dd1025be166292843615
smontante <shaunmontante@gmail.com>
2019-08-07 21:32:10 -0700

    first commit

---
+++ db/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+/*
+
+NOTES:
+1.) When connecting make sure you mongodb server is running, by running the script in the terminal below
+    /Users/admin/mongodb/bin/mongod --dbpath /Users/admin/mongodb-data
+*/
+
+
---
+++ server/app.js
@@ -0,0 +1,27 @@
+require('../db/mongoose')
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+const Contact = require('../models/contact')
+const publicDirectioryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectioryPath))
+app.use(express.json());
+
+app.post('/contact', (req, res)=> {
+    const contact = new Contact(req.body);
+    console.log('lead:', req.body)
+    ticket.save().then(()=> {
+        res.status(201).send(contact)
+    }).catch((e)=> {
+        res.status(400).send(e)
+    })
+})
+
+app.listen(port, ()=> {
+    console.log('listening on port ', port)
+})
+
+
+
