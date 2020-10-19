2b30e56bcce5b220e445acbc5fab88d0f88559d2
Shaun Montante <shaunmontante@gmail.com>
2020-10-19 10:56:25 -0700

    added 2 new forms for flip and contact us

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,53 @@
 // Get the modal
-var modal = document.getElementById("myModal");
-var modal2 = document.getElementById('rentalModal');
+var mainModal = document.getElementById("myModal");
+var rentalModal = document.getElementById('rentalModal');
+let flipModal = document.querySelector('#flipModal');
+let contactUsModal = document.querySelector('#contactUsModal');
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-var rentalButton = document.getElementById('rentalBtn');
+var rentalBtn = document.getElementById('rentalBtn');
+var flipBtn = document.getElementById('flipBtn')
+let learnMoreBtn = document.querySelector('#learnMoreBtn');
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 var span2 = document.getElementsByClassName("close")[1];
+var flipSpan = document.getElementsByClassName("close")[2];
+let contactSpan = document.getElementsByClassName("close")[3];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+  mainModal.style.display = "block";
 }
 
-rentalButton.onclick = function() {
-  modal2.style.display = "block";
+rentalBtn.onclick = function() {
+  rentalModal.style.display = "block";
+}
+
+flipBtn.onclick = function() {
+  flipModal.style.display = "block";
+}
+
+learnMoreBtn.onclick = function() {
+  contactUsModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  mainModal.style.display = "none";
 }
 
 span2.onclick = function() {
-  modal2.style.display = "none";
+  rentalModal.style.display = "none";
+}
+
+flipSpan.onclick = function() {
+  flipModal.style.display = "none";
+}
+
+contactSpan.onclick = function() {
+  contactUsModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
